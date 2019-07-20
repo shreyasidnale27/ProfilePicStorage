b4fa2958912c58b180a2cf2f2068c4190e74d80e
shreyasidnale27 <48942128+shreyasidnale27@users.noreply.github.com>
2019-07-20 19:07:14 +0530

    Profile pic

---
+++ XamarinStorage/XamarinStorage/Helper/FirebaseHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Firebase.Storage;
+
+namespace XamarinStorage.Helper
+{
+   public class FirebaseHelper
+    {
+        FirebaseStorage firebaseStorage= new FirebaseStorage("xamfirebase-910d9.appspot.com");
+
+       /* // Get a reference to the storage service, using the default Firebase App
+        Firebase.Storage.FirebaseStorage storage = Firebase.Storage.FirebaseStorage.DefaultInstance;
+
+        // Create a storage reference from our storage service
+        Firebase.Storage.StorageReference storage_ref =
+          storage.GetReferenceFromUrl("gs://imagesstorage-5338e.appspot.com");*/
+
+       /// <summary>
+        /// Uploads the file.
+        /// </summary>
+        /// <param name="fileStream">The file stream.</param>
+        /// <param name="fileName">Name of the file.</param>
+        /// <returns></returns>
+        public async Task<string> UploadFile(Stream fileStream, string fileName)
+        {
+            var imageUrl = await firebaseStorage
+                .Child("Images")
+                .Child(fileName)
+                .PutAsync(fileStream);
+            return imageUrl;
+        }
+
+        /// <summary>
+        /// Gets the file.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <returns></returns>
+        public async Task<string> GetFile(string fileName)
+        {
+            return await firebaseStorage
+                .Child("Images")
+                .Child(fileName)
+                .GetDownloadUrlAsync();
+        }
+
+        public async Task DeleteFile(string fileName)
+        {
+            await firebaseStorage
+                 .Child("Images")
+                 .Child(fileName)
+                 .DeleteAsync();
+        }
+    }
+}
---
+++ XamarinStorage/XamarinStorage/MainPage.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using XamarinStorage.Helper;
+using Xamarin.Forms;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System.Diagnostics;
+using System.IO;
+
+namespace XamarinStorage
+{
+    public partial class MainPage : ContentPage
+    {
+        /// <summary>
+        /// The fire storage
+        /// </summary>
+        FirebaseHelper fireStorage = new FirebaseHelper();
+
+        /// <summary>
+        /// The file is instance of media file
+        /// </summary>
+        MediaFile file;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainPage"/> class.
+        /// </summary>
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// When overridden, allows application developers to customize behavior immediately prior to the <see cref="T:Xamarin.Forms.Page" /> becoming visible.
+        /// </summary>
+        /// <remarks>
+        /// To be added.
+        /// </remarks>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        /// <summary>
+        /// Handles the Clicked event of the BtnPick control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private async void BtnPick_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            try
+            {
+                file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                {
+                    PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+                });
+                if (file == null)
+                    return;
+                imgChoosed.Source = ImageSource.FromStream(() =>
+                {
+                    var imageStram = file.GetStream();
+                    return imageStram;
+                });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Handles the Clicked event of the BtnUpload control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private async void BtnUpload_Clicked(object sender, EventArgs e)
+        {
+               await fireStorage.UploadFile(file.GetStream(), Path.GetFileName(file.Path));
+        }
+
+        private async void BtnDownload_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = await fireStorage.GetFile(txtFileName.Text);
+                if (path != null)
+                {
+                    lblPath.Text = path;
+                    await DisplayAlert("Success", path, "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private async void BtnDelete_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await fireStorage.DeleteFile(txtFileName.Text);
+                lblPath.Text = string.Empty;
+                await DisplayAlert("Success", "Deleted", "OK");
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
